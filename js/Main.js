750e94b81ccf7a32b53eb2b621299b7c0526bf6c
XuJoe <jxujoe@163.com>
2021-09-06 18:49:23 +0800

    first commit

---
+++ js/Main.js
@@ -0,0 +1,37 @@
+﻿
+var sVue = new Vue({
+  el: '#app',
+  data: {
+    object: {
+      name: '菜鸟教程',
+      url: 'http://www.runoob.com',
+      slogan: '学的不仅是技术，更是梦想！'
+    },
+    list: []
+  }
+  ,
+  methods: {
+    GetData: function () {
+      $.get('http://xujoe.com:9090/GetSend?SendKey=c786265a55').then(function (res) {
+        document.write(res.body);
+        this.list = res.Data;
+
+        console.log(this.list);
+      }, function () {
+        console.log('请求失败处理');
+      });
+    }
+
+
+
+
+  }
+
+  ,
+  created: function () {
+
+    console.log("created加载完毕")
+
+  }
+})
+
